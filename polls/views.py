f14224ce27b6f556d32702fb85ddc382541226d2
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-05 09:52:08 -0400

    broke my practice, will try to fix it after I finish my homework

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 
